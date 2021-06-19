f81b8b4ecdd8f8a0f8c1dd19522948c26406e413
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-06-19 19:19:03 +0100

    commit

--- src/helpers/StaticHelpers.java
+++ src/helpers/StaticHelpers.java
@@ -4,24 +4,13 @@ import java.util.Arrays;
 
 public class StaticHelpers {
 
-    public static void pT(Object o){
+
+    public static void p(Object o){ System.out.println(o); }
+    public static void pt(Object o){
+        //String classNameShort=o.getClass().getSimpleName();
         String className =o.getClass().getName();
         System.out.println(o + " ("+ className+")");
     }
-    public static void p(Object o,Boolean... printClassName){
-        //String classNameShort=o.getClass().getSimpleName();
-        String className =o.getClass().getName();
-        String stringToAppend = printClassName.length>0? " ("+ className+")" : "";
-        System.out.println(o + stringToAppend);
-    }
-    // public static void p(Object ... objects){
-    //     Arrays.stream(objects).forEach(o-> System.out.print(o));
-    //     System.out.println();
-    // }
-
-
-
-
     public static void pf(String s, Object ...o){
         System.out.printf(s,o);
         System.out.println();
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,12 +5,14 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import static helpers.StaticHelpers.p;
+import static helpers.StaticHelpers.pt;
 
 //class Main_{ public static void main(String [] args) { }}
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println() = p();
-//test
+        //System.out.println() = p();   with type: pt(), formatted: pf()
+
+        //this is template class
     }
 }
