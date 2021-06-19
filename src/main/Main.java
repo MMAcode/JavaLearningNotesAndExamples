728c57a55af8a8f440ea63b61745590787a5a048
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-06-19 15:51:11 +0100

    commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
 class Main_{ public static void main(String [] args) { }}
+//protected class Main_{ public static void main(String [] args) { }}
+//private class Main_{ public static void main(String [] args) { }}
+
+interface i1{}
+//private interface i2{}
 
 public class Main {
+    class someClass1 {}
+    private class someClass2 {}
+    protected class someClass3 {}
+    public class someClass4 {}
+    interface i2{} //public by default
+    private interface i3{}
+    protected interface i4{}
+
     public static void main(String[] args) {
         System.out.println("ahoj");
     }
