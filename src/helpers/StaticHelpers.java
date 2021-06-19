0bd0ac865351bd3039947109110d3f89eb411a81
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-06-19 13:09:25 +0100

    commit

---
+++ src/helpers/StaticHelpers.java
@@ -0,0 +1,15 @@
+package helpers;
+
+public class StaticHelpers {
+    public static void p(Object o,Boolean... printClassName){
+//        String classNameShort=o.getClass().getSimpleName();
+        String className =o.getClass().getName();
+        String stringToAppend = printClassName.length>0? " ("+ className+")" : "";
+        System.out.println(o + stringToAppend);
+    }
+    public static void pf(String s, Object ...o){
+        System.out.printf(s,o);
+        System.out.println();
+    }
+
+}
