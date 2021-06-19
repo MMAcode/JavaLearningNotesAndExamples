67798841aca2363dc78b70b24f10f4c9e1b95c34
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-06-19 15:41:33 +0100

    commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,8 @@
 package main;
 
+class Main_{ public static void main(String [] args) { }}
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("ahoj");
     }
