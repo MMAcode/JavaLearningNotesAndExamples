36f62c42a8e38ffeb490698300805a2fc8fdf280
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-06-24 17:29:08 +0100

    commit

--- src/Testing/Tester.java
+++ src/Testing/Tester.java
@@ -5,7 +5,7 @@ import main.Main;
 public class Tester {
     public static void main(String[] args) {
         Main m = new Main();
-        int x = m.hiding.v;
+//        int x = m.hiding.v;
 
     }
 }
