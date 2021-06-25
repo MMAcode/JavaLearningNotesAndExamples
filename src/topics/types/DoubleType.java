09dacdafbde7569634b9920ecba0c943e7ece170
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-06-25 10:39:33 +0100

    commit

--- src/topics/types/DoubleType.java
+++ src/topics/types/DoubleType.java
@@ -4,6 +4,13 @@ import static helpers.staticHelpers.TypeRecognizer.printTypeOf;
 
 public class DoubleType {
     public static void main(String[] args) {
+
+        double d = 23.45;
+        int i = 23;
+        System.out.println(i%2);
+        System.out.println(d%2);
+
+
         printTypeOf(Double.parseDouble("11"));
         printTypeOf(Double.valueOf("22"));
         printTypeOf(Double.valueOf(33));
