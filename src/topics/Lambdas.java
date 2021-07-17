45e29e41375e1f7319315852effac07d7fa3d192
MMAcode <52301171+MMAcode@users.noreply.github.com>
2021-07-17 22:18:43 +0100

    commit

---
+++ src/topics/Lambdas.java
@@ -0,0 +1,10 @@
+package topics;
+
+public class Lambdas {
+    public static void main(String[] args) {
+//        Object nr = new Object(){public int number=10;}.number
+        class Numr { int nr;}
+        Numr number = new Numr();
+        number.nr=10;
+    }
+}
