8d1230e0e18166a3c759e3d58c6adab93d7c0877
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-08-21 13:49:13 +0100

    commit

--- src/topics/parentChild/Main.java
+++ src/topics/parentChild/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Parent p = c;
         p.getAge(); //child's method activated here -> child's props used
         System.out.println(p.age);
+    //
 
     // conclusion: if child casted to parent and method called, method of the child is used.
         // If method is accessing properties, if child's method is activated, it will acess child's properties (if child class has that property);
